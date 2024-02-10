9e263606f981c1d041746f07fc82e6f2f288512d
HunMartin2024 <ratkaimartin2021@gmail.com>
2024-02-10 19:22:55 +0100

    Backend létrehozva Regisztrációnak és Loginnak

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,65 @@
+require('dotenv').config();
 const express = require('express');
+var mysql = require('mysql');
+const cors = require('cors');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const port = process.env.PORT;
+const bcrypt = require('bcrypt');
+const { restart } = require('nodemon');
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+// MIDDLEWARE FUNCION
+app.use(express.urlencoded({extended: true}));
+app.use(cors());
+app.use(express.json());
+
+var pool  = mysql.createPool({
+    connectionLimit : 10,
+    host            : process.env.DBHOST,
+    user            : process.env.DBUSER,
+    password        : process.env.DBPASS,
+    database        : process.env.DBNAME,
+    timezone: 'UTC'
 });
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+app.get('/', function (req, res) {
+    res.send('AutoAlibi Alkatreszek');
+});
+
+app.post('/register', async function (req, res){
+    let {name, email, password1, password2} = req.body
+    if(!name || !email || !password1 || !password2) return res.status(400).send({error: "Hiányzó adatok!"})
+    if(password1 != password2) return res.status(400).send({error: "Két jelszó nem egyezik!"})
+
+    const salt = await bcrypt.genSalt();
+    const hash = await bcrypt.hash(password1, salt);
+
+    pool.query(`INSERT INTO users(name, email, password) VALUES ('${name}','${email}','${hash}')`, (err, results)=>{
+        if(err) {
+            console.error(err);
+            res.status(500).send({error: "Adatbázis hiba!"})
+            return
+        }
+        res.send({msg: "Sikeres regisztráció!"})
+    })
+})
+
+app.post('/login', async function(req, res){
+    let {email, password} = req.body
+    if(!email || !password) return res.status(400)({error: "Hiányzó adatok!"})
+
+    pool.query(`SELECT * FROM users WHERE (email = '${email}')`, async (err, results)=>{
+        if(err) {
+            console.error(err);
+            res.status(500).send({error: "Adatbázis hiba!"})
+            return
+        }
+        if(!results[0]) return res.status(400).send({error: "Hibás e-mail/jelszó!"});
+        const compare = await bcrypt.compare(password, results[0].password);
+        if(!compare) return res.status(400).send({error: "Hibás e-mail/jelszó!"});
+        res.send({msg: "Sikeres bejelentkezés"})
+    })
+})
+
+  app.listen(port, () => {
+    console.log(`Server listening on port ${port}...`);
 });
--- frontend/Assets/js/app.js
+++ frontend/Assets/js/app.js
@@ -2,15 +2,18 @@ var app = angular.module('AutoAlibi', ['ngRoute']);
 
 app.run(function($rootScope){
     $rootScope.appTitle = 'AutoAlibi Alkatreszek';
+    $rootScope.backendURL = 'http://localhost:3002';
 });
 
 app.config(function($routeProvider){
     $routeProvider
     .when('/', {
-        templateUrl: 'Views/login.html'
+        templateUrl: 'Views/login.html',
+        controller: 'loginCtrl'
     })
     .when('/register', {
-        templateUrl: 'Views/register.html'
+        templateUrl: 'Views/register.html',
+        controller: 'registerCtrl'
     })
     .when('/newPass', {
         templateUrl: 'Views/newPassword.html'
