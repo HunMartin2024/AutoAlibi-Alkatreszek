0f4b5f0eb22dba33a898272ccf15d6c618fbd0a1
Martin <144666335+HunMartin2024@users.noreply.github.com>
2024-04-11 10:06:08 +0200

    AA-4 Új jelszó kérelem mezők javítva

--- frontend/Controllers/newPassCtrl.js
+++ frontend/Controllers/newPassCtrl.js
@@ -1,6 +1,19 @@
 app.controller('newPassCtrl', function($scope, $rootScope, $location){
     $scope.passwords = {}
     $scope.messages = []
+    $scope.passVisible = "password";
+    $scope.icon = "fa-eye";
+    $scope.passToggle =  function(){
+        if($scope.passVisible == "password")
+        {
+            $scope.passVisible = "text"
+            $scope.icon = "fa-eye-slash";
+        }
+        else{
+            $scope.passVisible = "password"
+            $scope.icon = "fa-eye";
+        }
+    }
     $scope.changePwd = async function(){
         $scope.messages = []
         try {
